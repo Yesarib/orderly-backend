0a75aa79ae606952be6e1a96ecbdf037e4c56c00
Barış Yesari <yesaribaris23@gmail.com>
2024-12-19 15:59:36 +0300

    fix: error response

--- src/common/dto/response.dto.ts
+++ src/common/dto/response.dto.ts
@@ -1,9 +1,9 @@
 export class ApiResponseDto<T> {
     success: boolean;
     data: T | null;
-    error?: string;
+    error?: T;
   
-    constructor(success: boolean, data: T | null, error?: string) {
+    constructor(success: boolean, data: T | null, error?: T) {
       this.success = success;
       this.data = data;
       if (error) this.error = error;
