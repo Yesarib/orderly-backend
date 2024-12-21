b9c026b1547b916016484ae47b2b053b7d6b92a4
Barış Yesari <yesaribaris23@gmail.com>
2024-12-21 17:01:54 +0300

    add mongoose configs to table module

--- src/table/table.module.ts
+++ src/table/table.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TableService } from './table.service';
 import { TableController } from './table.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TableModel, TableSchema } from './table.model';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: TableModel.name, schema: TableSchema }])
+  ],
   providers: [TableService],
   controllers: [TableController]
 })
-export class TableModule {}
+export class TableModule { }
